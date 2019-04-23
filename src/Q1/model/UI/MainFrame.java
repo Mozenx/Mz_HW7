e78a4fc064607555e38e5a33fe1904cd37dc84fa
mohsen <mim_z@hotmail.com>
2019-04-23 14:43:00 +0430

    create MainFram class and added some component of swing like button, frame to it

---
+++ src/Q1/model/UI/MainFrame.java
@@ -0,0 +1,43 @@
+package Q1.model.UI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame {
+
+    static JFrame frame;
+    JButton stB, tchB;
+
+    public MainFrame(){
+        //declare some components and set state of them
+        frame = new JFrame();
+        stB = new JButton("Student");
+        tchB = new JButton("Teacher");
+        stB.setBounds(50,20,100,20);
+        tchB.setBounds(50,50,100,20);
+
+        //add components to the frame
+        frame.setSize(200,130);
+        frame.setLayout(null);
+        frame.add(tchB);
+        frame.add(stB);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //add action to buttons
+        stB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        tchB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
