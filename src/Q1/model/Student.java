96f24480d1ca5ee67c94b083218cdc091f48b1b8
mohsen <mim_z@hotmail.com>
2019-04-23 11:35:54 +0430

    added to Student and Teacher class cunstructor, setter and getter

--- src/Q1/model/Student.java
+++ src/Q1/model/Student.java
@@ -1,4 +1,35 @@
 package Q1.model;
 
 public class Student {
+    private String fName, lname, sId;
+
+    public Student(String fName, String lname, String sId) {
+        this.fName = fName;
+        this.lname = lname;
+        this.sId = sId;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
 }
--- src/Q1/model/Teacher.java
+++ src/Q1/model/Teacher.java
@@ -1,4 +1,35 @@
 package Q1.model;
 
 public class Teacher {
+    private String fName, lName, tId;
+
+    public Teacher(String fName, String lName, String tId) {
+        this.fName = fName;
+        this.lName = lName;
+        this.tId = tId;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String gettId() {
+        return tId;
+    }
+
+    public void settId(String tId) {
+        this.tId = tId;
+    }
 }
