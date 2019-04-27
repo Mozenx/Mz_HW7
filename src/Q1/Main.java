96a91eb7d6c080c8ae64650497d92f02ec74ad05
mohsen <mim_z@hotmail.com>
2019-04-27 13:47:19 +0430

    pulish

--- src/Q1/Main.java
+++ src/Q1/Main.java
@@ -6,12 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //declare two treeSets
         TreeSet<Character> test1 = new TreeSet<>();
         TreeSet<Character> test2 = new TreeSet<>();
 
+        //fill and print two declared treeSets
         System.out.println("TreeSet 1:\n"+TreeSetExample.addChar(test1));
         System.out.println("TreeSet 2:\n"+TreeSetExample.addChar(test2));
 
+        //use method of TreeSetExample
         TreeSetExample.findUnion(test1,test2);
         TreeSetExample.findIntersection(test1,test2);
         TreeSetExample.find(test1, test2);
