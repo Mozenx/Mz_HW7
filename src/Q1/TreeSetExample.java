959a87c16e61560f254cd14972bbb718d169fb0e
mohsen <mim_z@hotmail.com>
2019-04-27 13:35:22 +0430

    added findIntersection method to TreeSetExample calss

--- src/Q1/TreeSetExample.java
+++ src/Q1/TreeSetExample.java
@@ -1,5 +1,7 @@
 package Q1;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.TreeSet;
 
@@ -27,4 +29,26 @@ public class TreeSetExample {
         System.out.println("\nUnion of two sets:\n"+addTree);
 
     }//end of findUnion
+
+    //findIntersection method for finding Intersection of two sets with ArrayList.
+    public static void findIntersection(TreeSet<Character> tree1, TreeSet<Character> tree2){
+
+        List<Character> coll1 = new ArrayList<>();
+        List<Character> coll2 = new ArrayList<>();
+        TreeSet<Character> treeSet = new TreeSet<>();
+        coll1.addAll(tree1);
+        coll2.addAll(tree2);
+        for(int i = 0; i < coll1.size(); i++){
+
+            for(int j = 0; j<coll1.size();j++){
+
+                if(coll1.get(i) == coll2.get(j)){
+                    treeSet.add(coll1.get(i));
+                }
+            }
+        }//end of for
+
+        System.out.println("\nIntersection of two sets used ArrayList.\n"+treeSet);
+
+    }//end of findCommon method
 }
