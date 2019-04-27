23e7664f7fd79ee68ee87d4d9d6358cae8c29dd0
mohsen <mim_z@hotmail.com>
2019-04-27 13:34:09 +0430

    Added addCahr method for creating TreeSet with random char to TreeSetExample class

--- src/Q1/TreeSetExample.java
+++ src/Q1/TreeSetExample.java
@@ -1,8 +1,21 @@
 package Q1;
 
+import java.util.Random;
 import java.util.TreeSet;
 
 public class TreeSetExample {
+
+
+    //adding 10 random char to the TreeSet object
+    public static TreeSet<Character> addChar(TreeSet<Character> treeSet){
+
+        Random rn = new Random();
+        while (!(treeSet.size() == 10)){
+            char ch = (char)(rn.ints(97,122+1).findFirst().getAsInt());
+            treeSet.add(ch);
+        }
+        return treeSet;
+    }//end of addChar method
 
     //findUnion method for finding Union of two sets.
     public static void findUnion(TreeSet<Character> tree1, TreeSet<Character> tree2){
