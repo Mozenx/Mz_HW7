fee70868f2ba4caadc067706525ff89446e6236a
mohsen <mim_z@hotmail.com>
2019-04-27 13:32:08 +0430

    Added findUnion method to TreeSetExample

--- src/Q1/TreeSetExample.java
+++ src/Q1/TreeSetExample.java
@@ -1,4 +1,17 @@
 package Q1;
 
+import java.util.TreeSet;
+
 public class TreeSetExample {
+
+    //findUnion method for finding Union of two sets.
+    public static void findUnion(TreeSet<Character> tree1, TreeSet<Character> tree2){
+
+        TreeSet<Character> addTree = new TreeSet<>();
+        addTree.addAll(tree1);
+        addTree.addAll(tree2);
+
+        System.out.println("\nUnion of two sets:\n"+addTree);
+
+    }//end of findUnion
 }
