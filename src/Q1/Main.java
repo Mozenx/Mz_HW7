32416b2a28cb1fbfded7c7e4b32dc95f6e57fc50
mohsen <mim_z@hotmail.com>
2019-04-27 13:43:10 +0430

    added two treeset to main  fill and print them

--- src/Q1/Main.java
+++ src/Q1/Main.java
@@ -1,9 +1,17 @@
 package Q1;
 
+import java.util.TreeSet;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        TreeSet<Character> test1 = new TreeSet<>();
+        TreeSet<Character> test2 = new TreeSet<>();
+
+        System.out.println("TreeSet 1:\n"+TreeSetExample.addChar(test1));
+        System.out.println("TreeSet 2:\n"+TreeSetExample.addChar(test2));
+
     }
 
 }//end of class
